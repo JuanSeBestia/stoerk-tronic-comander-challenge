ab64540706d3b21630e85527f712218ca84b6721
JuanSe Dussan <dussan@stoerk-tronic.com>
2021-06-08 12:32:55 -0500

    change label

--- src/modules/Commander/components/Charts/chartSeries.tsx
+++ src/modules/Commander/components/Charts/chartSeries.tsx
@@ -25,7 +25,7 @@ export const chartSeries = [
     ChartComponent: RangeTime,
   },
   {
-    name: "STGoogleSeries(Not work yet)",
+    name: "STGoogleSeries",
     path: "line/STGoogleSeries",
     ChartComponent: STGoogleSeries,
   },
